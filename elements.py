27d481603b65e9da20859c1a904592ef0ea19a8d
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-04 11:27:42 -0500

    Update elements.py

--- elements.py
+++ elements.py
@@ -2,34 +2,37 @@ element = ["Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","Nitrogen"
 number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109]
 group = [1,18,1,2,13,14,15,16,17,18,1,2,13,14,15,16,17,18,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,1,2,3,102,102,102,102,102,102,102,102,102,102,102,102,102,102,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,1,2,3,102,102,102,102,102,102,102,102,102,102,102,102,102,103,4,5,6,7,8,9]
 valence = []
+
 twopers = [2,3,4,7,12,102]
+othertwos = [23,73,105,74,106,26,76,108,27,77,109,28,110,111]
+oners = [41,24,42,44,45,78,29,47,79]
 others = [14,15,16,17,18]
 
 pairings = []
 
 for i in range(len(number)-1):
     pairings.append([element[i], number[i], group[i]])
+# [5,6,8,9,10,11,]
 
-
-for i in group:
-    if i == 1:
+for i in range(len(group)):
+    if group[i] == 1:
         valence.append(1)
-    elif i == 103 or i == 13:
+    elif group[i] == 103 or group[i] == 13:
         valence.append(3)
-    elif group[i] == 14:
-        valence.append(4)
+    elif i == 46:
+        valence.append(18)
+    for f in oners:
+        if number[i] == f:
+            valence.append(1)
+    for g in othertwos:
+        if number[i] == g:
+            valence.append(2)
     for j in twopers:
-        if i == j:
+        if group[i] == j:
             valence.append(2)
     for h in range(len(others)):
-        if i == others[h]:
+        if group[i] == others[h]:
             valence.append(h+4)
     
-print(valence, len(group), len(valence))
-"""
-for i in range(len(group)-1):
-    if group[i] == 18:
-        group[i] = 8
+#print (valence, len(group), len(valence))
 
-print(group)
-"""
